ba779a565eaea7863e83f8356dbd8b71af54b2fb
Kristina Nylander <kristina.nylander@mittmedia.se>
2020-12-11 18:36:33 +0100

    Cleanup

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -2,21 +2,25 @@ import scala.annotation.tailrec
 import scala.io.Source
 
 object Day11 {
+
+  case class Coord(x: Int, y: Int)
+  type NMap = Map[Coord, Char]
+
   @tailrec
-  def parse(lines: List[String], map: Map[(Int, Int), Char] = Map.empty, y: Int = 0): Map[(Int, Int), Char] = {
+  def parse(lines: List[String], map: NMap = Map.empty, y: Int = 0): NMap = {
     if (lines.isEmpty)
       return map
 
-    val newMap = lines.head.indices.map(i => (i, y) -> lines.head(i)).toMap
+    val newMap = lines.head.indices.map(i => Coord(i, y) -> lines.head(i)).toMap
     parse(lines.tail, map ++ newMap, y + 1)
   }
 
-  def printMap(map: Map[(Int, Int), Char]): Unit = {
-    val xMax = map.keys.map { case (x, _) => x }.max
-    val yMax = map.keys.map { case (_, y) => y }.max
+  def printMap(map: NMap): Unit = {
+    val xMax = map.keys.map(_.x).max
+    val yMax = map.keys.map(_.y).max
     for (y <- 0 to yMax) {
       for (x <- 0 to xMax) {
-        print(map(x, y))
+        print(map(Coord(x, y)))
       }
       println()
     }
@@ -24,8 +28,8 @@ object Day11 {
   }
 
   @tailrec
-  def solve(map: Map[(Int, Int), Char], neighbourFun: (Int, Int, Map[(Int, Int), Char]) => List[(Int, Int)], maxNeighbours: Int, lastState: Set[(Int, Int)] = Set.empty): Int = {
-    val updatedMap = map.keys.map { case (x, y) => nextState(x, y, map, neighbourFun, maxNeighbours) }.toMap
+  def solve(map: NMap, neighbourFun: (Coord, NMap) => List[Coord], maxNeighbours: Int, lastState: Set[Coord] = Set.empty): Int = {
+    val updatedMap = map.keys.map(nextState(_, map, neighbourFun, maxNeighbours)).toMap
     val keySet = updatedMap.filter { case (_, c) => c == '#' }.keys.toSet
     if (keySet == lastState)
       updatedMap.count { case (_, c) => c == '#' }
@@ -33,51 +37,52 @@ object Day11 {
       solve(updatedMap, neighbourFun, maxNeighbours, keySet)
   }
 
-  def nextState(x: Int, y: Int, map: Map[(Int, Int), Char], neighbourFun: (Int, Int, Map[(Int, Int), Char]) => List[(Int, Int)], maxNeighbours: Int): ((Int, Int), Char) = {
-    val neighbours = neighbourFun(x, y, map)
-    map(x, y) match {
-      case 'L' => toOccupied(x, y, neighbours, map)
-      case '#' => toFree(x, y, maxNeighbours, neighbours, map)
-      case '.' => (x, y) -> '.'
+  def nextState(coord: Coord, map: NMap, neighbourFun: (Coord, NMap) => List[Coord], maxNeighbours: Int): (Coord, Char) = {
+    val neighbours = neighbourFun(coord, map)
+    map(coord) match {
+      case 'L' => toOccupied(coord, neighbours, map)
+      case '#' => toFree(coord, maxNeighbours, neighbours, map)
+      case '.' => coord -> '.'
     }
   }
 
-  def toOccupied(x: Int, y: Int, neighbours: List[(Int, Int)], map: Map[(Int, Int), Char]): ((Int, Int), Char) = {
-    val allNeighboursFree = !neighbours.exists { case (x, y) => map.getOrElse((x, y), '.') == '#' }
+  def toOccupied(coord: Coord, neighbours: List[Coord], map: NMap): (Coord, Char) = {
+    val allNeighboursFree = !neighbours.exists(map.getOrElse(_, '.') == '#')
     if (allNeighboursFree)
-      (x, y) -> '#'
+      coord -> '#'
     else
-      (x, y) -> 'L'
+      coord -> 'L'
   }
 
-  def toFree(x: Int, y: Int, maxNeighbours: Int, neighbours: List[(Int, Int)], map: Map[(Int, Int), Char]): ((Int, Int), Char) = {
-    val occupiedNeighbours = neighbours.count { case (x, y) => map.getOrElse((x, y), '.') == '#' }
+  def toFree(coord: Coord, maxNeighbours: Int, neighbours: List[Coord], map: NMap): (Coord, Char) = {
+    val occupiedNeighbours = neighbours.count(map.getOrElse(_, '.') == '#')
     if (occupiedNeighbours >= maxNeighbours)
-      (x, y) -> 'L'
+      coord -> 'L'
     else
-      (x, y) -> '#'
+      coord -> '#'
   }
 
-  def getNeighbours(x: Int, y: Int, map: Map[(Int, Int), Char]): List[(Int, Int)] = {
-    List((x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
-      (x, y - 1), (x, y + 1),
-      (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)).filter(map.contains)
+  def getNeighbours(coord: Coord, map: NMap): List[Coord] = {
+    val x = coord.x
+    val y = coord.y
+    List(Coord(x - 1, y - 1), Coord(x - 1, y), Coord(x - 1, y + 1),
+      Coord(x, y - 1), Coord(x, y + 1),
+      Coord(x + 1, y - 1), Coord(x + 1, y), Coord(x + 1, y + 1)).filter(map.contains)
   }
 
-  def getNeighbours2(x: Int, y: Int, map: Map[(Int, Int), Char]): List[(Int, Int)] = {
-    List(find(x, y, -1, -1, map), find(x, y, -1, 0, map), find(x, y, -1, 1, map),
-      find(x, y, 0, -1, map), find(x, y, 0, 1, map),
-      find(x, y, 1, -1, map), find(x, y, 1, 0, map), find(x, y, 1, 1, map)).flatten
+  def getNeighbours2(coord: Coord, map: NMap): List[Coord] = {
+    List(find(coord, -1, -1, map), find(coord, -1, 0, map), find(coord, -1, 1, map),
+      find(coord, 0, -1, map), find(coord, 0, 1, map),
+      find(coord, 1, -1, map), find(coord, 1, 0, map), find(coord, 1, 1, map)).flatten
   }
 
   @tailrec
-  def find(x: Int, y: Int, dx: Int, dy: Int, map: Map[(Int, Int), Char]): Option[(Int, Int)] = {
-    val nextX = x + dx
-    val nextY = y + dy
-    if (map.contains((nextX, nextY))) {
-      map((nextX, nextY)) match {
-        case '.' => find(nextX, nextY, dx, dy, map)
-        case _ => Option((nextX, nextY))
+  def find(coord: Coord, dx: Int, dy: Int, map: NMap): Option[Coord] = {
+    val nextCoord = Coord(coord.x + dx, coord.y + dy)
+    if (map.contains(nextCoord)) {
+      map(nextCoord) match {
+        case '.' => find(nextCoord, dx, dy, map)
+        case _ => Option(nextCoord)
       }
     } else
       None
