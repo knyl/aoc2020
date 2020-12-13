6a2cb535e9ee4cc67902cbeee1baf2439d8bafbe
Kristina Nylander <kristina.nylander@mittmedia.se>
2020-12-13 14:13:04 +0100

    Cleanup

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -2,10 +2,6 @@ import scala.io.Source
 
 object Day13 {
 
-  private def getEarliestTime(time: Int)(t: Int): Int = {
-   t - (time % t)
-  }
-
   private def solve1(lines: List[String]): Int = {
     val time = lines.head.toInt
     val departures = lines.last.split(",").filter(_ != "x").map(_.toInt).sorted
@@ -16,15 +12,16 @@ object Day13 {
   /*
    * This method assumes that the input numbers are all prime numbers
    */
-  private def solve2(lines: List[String]): String = {
+  private def solve2(lines: List[String]): Long = {
     val departures = lines.last.split(",").zipWithIndex.filter(_._1 != "x").map(t => (t._1.toLong, t._2.toLong))
-    val res = departures.tail.foldLeft((departures.head._1, 0.toLong))((acc, el) => (acc._1 * el._1, findNextValue(acc._1, acc._2, el)))
-    res._2.toString
+    val res = departures.tail
+      .foldLeft((departures.head._1, 0.toLong))((acc, el) => (acc._1 * el._1, findNextValue(acc._1, acc._2, el)))
+    res._2
   }
 
   private def findNextValue(step: Long, start: Long, target: (Long, Long)): Long = {
     var currPos = start
-    while (((currPos + target._2) % target._1) != 0 ) {
+    while (((currPos + target._2) % target._1) != 0) {
       currPos += step
     }
     currPos
